aa23c5019b3fff485c49f760934c592108e06e1b
Gagan Dev P V <gagandevstm123@gmail.com>
2025-09-09 05:34:18 -0700

    Update EmailUtils.java

--- src/main/java/blockchain_ticketing_system/EmailUtils.java
+++ src/main/java/blockchain_ticketing_system/EmailUtils.java
@@ -5,39 +5,28 @@ import jakarta.mail.internet.*;
 import java.util.Properties;
 
 public class EmailUtils {
-
     private static final String SMTP_HOST = "smtp.gmail.com";
     private static final String SMTP_PORT = "587";
     private static final String EMAIL_USERNAME = "xxxxx@gmail.com";
-    // Read password from environment variable
     private static final String EMAIL_PASSWORD = "xxx xxx xxx xxx";
-
     public static void sendEmail(String to, String subject, String body) throws MessagingException {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", SMTP_HOST);
         props.put("mail.smtp.port", SMTP_PORT);
-
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(EMAIL_USERNAME, EMAIL_PASSWORD);
             }
         });
-
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(EMAIL_USERNAME));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
         message.setSubject(subject);
         message.setText(body);
-
         Transport.send(message);
     }
 
-    // Optional main to test email sending
-    public static void main(String[] args) throws Exception {
-        sendEmail("recipient@example.com", "Test Email", "This is a test.");
-        System.out.println("Email sent!");
-    }
 }
